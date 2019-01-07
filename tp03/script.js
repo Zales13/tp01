80c5101cc9c5a082711e09bd3d81bde18e371bde
Issa <dxissa@gmail.com>
2019-01-07 17:54:20 +0100

    pas finis

--- tp03/script.js
+++ tp03/script.js
@@ -1,5 +1,16 @@
+var ctx = document.getElementById("myChart").getContext('2d');
 var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: options
+
+type: 'line',
+data: {
+    labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet"],
+    datasets: [{
+        label: "Mes premieres Donnés",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+},
+
+options: {}
 });
